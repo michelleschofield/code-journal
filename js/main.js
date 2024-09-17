6f3a983116ecab8e638deb38e7efc5688dadefbe
Michelle Schofield <schofieldshelly05@gmail.com>
2024-09-17 16:25:10 +0000

    Listen for input in url field and set photo url to that

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
+'use strict';
 const $photoURL = document.querySelector('#photo-url');
 const $photo = document.querySelector('img');
-
 if (!$photoURL) throw new Error('$photoURL query failed');
 if (!$photo) throw new Error('$photo query failed');
-
-$photoURL.addEventListener('input', (event: Event) => {
-  const $eventTarget = event.target as HTMLInputElement;
+$photoURL.addEventListener('input', (event) => {
+  const $eventTarget = event.target;
   const url = $eventTarget.value;
   $photo.setAttribute('src', url);
 });
