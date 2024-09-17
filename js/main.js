17c53b8ee991ff5ddbe14aa4f034824c4bdcfdb4
Michelle Schofield <schofieldshelly05@gmail.com>
2024-09-17 16:43:41 +0000

    Listen for submit and store values in object

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 'use strict';
 const $photoURL = document.querySelector('#photo-url');
 const $photo = document.querySelector('img');
+const $form = document.querySelector('form');
 if (!$photoURL) throw new Error('$photoURL query failed');
 if (!$photo) throw new Error('$photo query failed');
+if (!$form) throw new Error('$form query failed');
 $photoURL.addEventListener('input', (event) => {
   const $eventTarget = event.target;
   const url = $eventTarget.value;
   $photo.setAttribute('src', url);
+});
+$form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (!$form) return;
+  const $formElements = $form.elements;
+  const title = $formElements.title.value;
+  const url = $formElements.url.value;
+  const notes = $formElements.notes.value;
+  const formValues = {
+    title,
+    url,
+    notes,
+  };
+  console.log('formValues', formValues);
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,37 @@
+interface FormElements extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  url: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
+
 const $photoURL = document.querySelector('#photo-url');
 const $photo = document.querySelector('img');
+const $form = document.querySelector('form');
 
 if (!$photoURL) throw new Error('$photoURL query failed');
 if (!$photo) throw new Error('$photo query failed');
+if (!$form) throw new Error('$form query failed');
 
 $photoURL.addEventListener('input', (event: Event) => {
   const $eventTarget = event.target as HTMLInputElement;
   const url = $eventTarget.value;
   $photo.setAttribute('src', url);
+});
+
+$form.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+  if (!$form) return;
+  const $formElements = $form.elements as FormElements;
+  const title = $formElements.title.value;
+  const url = $formElements.url.value;
+  const notes = $formElements.notes.value;
+
+  const formValues = {
+    title,
+    url,
+    notes,
+  };
+
+  console.log('formValues', formValues);
 });
